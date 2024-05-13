8ff6032c8b7b6bab5da857e5ed7676c8e9334944
aryan88111 <76144922+aryan88111@users.noreply.github.com>
2024-05-13 10:45:41 +0530

    updated 13/05/24

--- routes/forget.js
+++ routes/forget.js
@@ -1,12 +1,13 @@
 const express = require('express');
 const User = require('../model/user');
+const Member = require('../model/member');
 const router = express.Router();
 const bcrypt = require("bcrypt");
 
-router.post("/forget",async(req,res)=>{
+router.post("/userforget",async(req,res)=>{
   try{
     const userData=await User.findOne({email: req.body.email});
-    await userData.save()
+    // await userData.save()
     console.log(userData,"uuuuuuuu");
     if(userData){
       console.log("asdfghj");
@@ -20,6 +21,38 @@ router.post("/forget",async(req,res)=>{
           console.log(req.body);
           console.log(newPassword);
             const updatePassword=await User.findByIdAndUpdate(Id,{
+              $set:{passWord:newPassword}
+            });
+            await updatePassword.save();
+            await userData.save();
+            console.log(updatePassword,"rrrrrrrr");
+           return res.send("Password Updated")
+        }
+    }else{
+       return res.send("Please check your email")
+    }
+    
+  }catch{
+    res.status(505);
+  }
+});
+router.post("/memberforget",async(req,res)=>{
+  try{
+    const userData=await Member.findOne({email: req.body.email});
+    await userData.save()
+    console.log(userData,"uuuuuuuu");
+    if(userData){
+      console.log("asdfghj");
+      console.log(userData.answer1);
+      const Id=userData._id;
+      console.log(Id);
+        if(req.body.answer1==userData.answer1 && req.body.answer2==userData.answer2 && req.body.answer3==userData.answer3){
+          const newPassword = await bcrypt.hashSync(req.body.passWord, 10);
+          // const newPassword = await req.body.passWord;
+
+          console.log(req.body);
+          console.log(newPassword);
+            const updatePassword=await Member.findByIdAndUpdate(Id,{
               $set:{passWord:newPassword}
             });
             await updatePassword.save();
